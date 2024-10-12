0f488df59bf55d0f89abbf7d3fa413991af3da31
HazelDaniel <hazeldaniel856@gmail.com>
2024-10-12 23:00:35 +0100

    basics: formatted print

---
+++ doc/exercises/day-1/hello.rs
@@ -0,0 +1,7 @@
+fn main() {
+    /// this is awesome i guess
+    /// # Another program title
+    // eprintln!("Hello World");
+    println!("Hello {}", "Joe");
+    println!("{0}, meet {1}. {1}, meet {0}.", "Joe", "Jane");
+}
