32f46c943d797d36cd399a188fbbf69133cede45
HazelDaniel <hazeldaniel856@gmail.com>
2024-10-14 21:57:12 +0100

    learning how to implement the fmt::Display trait

--- doc/exercises/day-2/formatting.rs
+++ doc/exercises/day-2/formatting.rs
@@ -6,9 +6,21 @@ struct Object1(i32);
 #[derive(Debug)]
 struct TwoNumTuple(i32, i32);
 
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
 impl fmt::Display for TwoNumTuple {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         return write!(f, "[{}, {}]", self.0, self.1);
+    }
+}
+
+impl fmt::Display for Point2D {
+    fn fmt (&self, stream: &mut fmt::Formatter) -> fmt::Result {
+        return write!(stream, "{} + {}i", self.x, self.y);
     }
 }
 
@@ -19,5 +31,11 @@ fn main() {
 
     let numTuple1 = TwoNumTuple(20, 20);
 
+    let point1 = Point2D {x: 3.3, y: 7.2};
+
     println!("here are two 20s for you {}", numTuple1);
+
+    // ACTIVITY
+    println!("Debug: Complex {{ real: {}, imag: {} }}", point1.x, point1.y);
+    println!("Display: {}", point1);
 }
