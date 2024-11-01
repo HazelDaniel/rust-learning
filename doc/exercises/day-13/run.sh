afd41b1859edafb54ee562deda9d2d37016ad2b4
HazelDaniel <hazeldaniel856@gmail.com>
2024-11-01 21:35:44 +0100

    day-13: still learning how to use cargo. moved from using the build.rs script as it was problematic. should be able to solve the skill issue later

--- doc/exercises/day-13/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-#
-rustc ./libraries.rs --extern class=libclass.rlib -o ./libraries.out
-sleep 1
-# clear
-./libraries.out
--- doc/exercises/day-13/src/main.rs
+++ doc/exercises/day-13/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+
+mod classes {
+    pub mod     methods;
+}
+
+mod composers;
+mod functions;
+
+use functions::double;
+use composers::{compose_with_integer};
+
+use class::Person;
+
+
+fn main () -> () {
+    let mut alice: Person = Person::new(1, 1001);
+
+    println!("created alice of type {}", alice);
+
+    alice.update_SSN(2002021);
+
+    println!("alice is now of type {}", alice);
+
+    let halfer = |x| x / 2;
+
+    let same_8 = compose_with_integer(8, halfer, double);
+
+    println!("8 stays the same as {}..", same_8);
+
+    return ();
 }
