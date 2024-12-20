186b7f633cd2991e8590029887a966ef101dae8b
HazelDaniel <hazeldaniel856@gmail.com>
2024-12-20 04:39:42 +0100

    day-18: strings (contd)

--- doc/exercises/day-18/src/main.rs
+++ doc/exercises/day-18/src/main.rs
@@ -1,8 +1,30 @@
 #![allow(unused)]
 
-use utils::strings::conversion::{reverse_string, upper_string};
+use utils::strings::conversion::{
+    reverse_string, rotate_string_l, rotate_string_l_by, rotate_string_r, rotate_string_r_by,
+    upper_string,
+};
 
 fn main() {
     println!("(last word reversed) Hello, {:?}!", reverse_string("World"));
     println!("(last word upper) Hello, {:?}!", upper_string("World"));
+    println!(
+        "(last word rotated right) Hello, {:?}!",
+        rotate_string_r("World")
+    );
+
+    println!(
+        "(last word rotated right by 3) Hello, {:?}!",
+        rotate_string_r_by("World", 3)
+    );
+
+    println!(
+        "(last word rotated left) Hello, {:?}!",
+        rotate_string_l("World")
+    );
+
+    println!(
+        "(last word rotated left by 3) Hello, {:?}!",
+        rotate_string_l_by("World", 3)
+    );
 }
--- doc/exercises/day-18/utils/src/strings/module.rs
+++ doc/exercises/day-18/utils/src/strings/module.rs
@@ -1,7 +1,4 @@
 pub mod conversion {
-    #[derive(Debug)]
-    pub struct Animal;
-
     pub fn reverse_string(string: &str) -> String {
         let mut reversed = String::new();
 
@@ -13,9 +10,70 @@ pub mod conversion {
 
     pub fn upper_string(string: &str) -> String {
         let mut upper = String::new();
+
         for s in string.chars() {
             upper.push_str(&s.to_uppercase().to_string());
         }
         upper
+    }
+
+    pub fn rotate_string_r(string: &str) -> String {
+        let mut rotated_vec: Vec<char> = string.chars().collect();
+
+        for (i, s) in string.chars().enumerate() {
+            rotated_vec[(i + 1) % string.len()] = s;
+        }
+        let result: String = rotated_vec.into_iter().collect();
+
+        result
+    }
+
+    pub fn rotate_string_l(string: &str) -> String {
+        let mut rotated_vec: Vec<char> = string.chars().collect();
+
+        for (i, s) in string.chars().enumerate() {
+            let x: i16 = i as i16;
+
+            if x - 1 < 0 {
+                rotated_vec
+                    [(x + string.len() as i16 - 1).rem_euclid(string.len() as i16) as usize] = s;
+            } else {
+                rotated_vec[(x - 1).rem_euclid(string.len() as i16) as usize] = s;
+            }
+        }
+        let result: String = rotated_vec.into_iter().collect();
+
+        result
+    }
+
+    pub fn rotate_string_r_by(string: &str, rot: i16) -> String {
+        let mut rotated_vec: Vec<char> = string.chars().collect();
+
+        for (i, s) in string.chars().enumerate() {
+            let x: i16 = i as i16;
+
+            rotated_vec[(x + rot).rem_euclid(string.len() as i16) as usize] = s;
+        }
+        let result: String = rotated_vec.into_iter().collect();
+
+        result
+    }
+
+    pub fn rotate_string_l_by(string: &str, rot: i16) -> String {
+        let mut rotated_vec: Vec<char> = string.chars().collect();
+
+        for (i, s) in string.chars().enumerate() {
+            let x: i16 = i as i16;
+
+            if x - rot < 0 {
+                rotated_vec
+                    [(x + string.len() as i16 - rot).rem_euclid(string.len() as i16) as usize] = s;
+            } else {
+                rotated_vec[(x - rot).rem_euclid(string.len() as i16) as usize] = s;
+            }
+        }
+        let result: String = rotated_vec.into_iter().collect();
+
+        result
     }
 }
